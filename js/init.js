822326c1664eeb1c370c2e20a1336dfe3fe074fc
lingz <lingz@lingzdeMacBook-Pro.local>
2018-01-26 19:54:29 +0800

    动效页面

--- js/init.js
+++ js/init.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
         endingTop: '6%'
     });
 });
- 
 function radomChangeColor() {
     var color = ["purple lighten-3","light-blue lighten-2","cyan lighten-2","teal lighten-3","green lighten-2","orange lighten-3","deep-orange lighten-3"];
     var num = Math.floor(Math.random() * color.length);
