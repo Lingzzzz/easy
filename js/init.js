936174d82b90bca68e74fa7350bae577fd9385c6
lingz <lingz@lingzdeMacBook-Pro.local>
2018-01-31 08:58:49 +0800

    some chages

--- js/init.js
+++ js/init.js
@@ -13,6 +13,6 @@ function radomChangeColor() {
     var color = ["purple lighten-3","light-blue lighten-2","cyan lighten-2","teal lighten-3","green lighten-2","orange lighten-3","deep-orange lighten-3"];
     var num = Math.floor(Math.random() * color.length);
     console.log(color[num]);
-    $('#headerWithBG').addClass(color[num]);
+    $('#bgSnow').addClass(color[num]);
 }
 
